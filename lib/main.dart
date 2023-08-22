805680ce196491b87ca9dec26774eae264d8813a
estevaosampaio <estevaocarvalhogyn@gmail.com>
2023-08-21 22:38:48 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter First App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+             Text(
+              'Contador',
+              style: Theme.of(context).textTheme.displayMedium,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.displayLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Stack(
+        children: [
+          Positioned(
+            bottom: screenHeight * 0.1,
+            left: screenWidth * 0.1,
+            child: FloatingActionButton(
+              onPressed: _decrementCounter,
+              tooltip: 'First Button',
+              child: const Icon(Icons.remove),
+            ),
+          ),
+          Positioned(
+            bottom: screenHeight * 0.1,
+            right: screenWidth * 0.1,
+            child: FloatingActionButton(
+              onPressed: _incrementCounter,
+              tooltip: 'Second Button',
+              child: const Icon(Icons.add),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           const Text(
+  //             'Contador',
+  //           ),
+  //           Text(
+  //             '$_counter',
+  //             style: Theme.of(context).textTheme.headlineMedium,
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //     floatingActionButton: Stack(
+  //       children: [
+  //         Positioned(
+  //           bottom: 16.0,
+  //           left: 16.0,
+  //           child: FloatingActionButton(
+  //             onPressed: _decrementCounter,
+  //             tooltip: 'First Button',
+  //             child: const Icon(Icons.remove),
+  //           ),
+  //         ),
+  //         Positioned(
+  //           bottom: 16.0,
+  //           right: 16.0,
+  //           child: FloatingActionButton(
+  //             onPressed: _incrementCounter,
+  //             tooltip: 'Second Button',
+  //             child: const Icon(Icons.add),
+  //           ),
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           const Text(
+  //             'You have pushed the button this many times:',
+  //           ),
+  //           Text(
+  //             '$_counter',
+  //             style: Theme.of(context).textTheme.headlineMedium,
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: _incrementCounter,
+  //       tooltip: 'Increment',
+  //       child: const Icon(Icons.add),
+  //     ),
+  //   );
+  // }
